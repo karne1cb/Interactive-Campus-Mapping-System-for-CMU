1bbfeedce4063115f61e1de36e66ba9b16d12972
unknown <karne1cb@cmich.edu>
2023-04-28 10:33:56 -0400

    Refactored the App component slightly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,26 @@
 import './CSS/App.css';
 import './CSS/index.css';
-import { Route, BrowserRouter as Router, Routes, Navigate, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes, Navigate} from 'react-router-dom';
 import MapView from './Components/MapView';
 import Login from './Components/Login';
-import AdminTest from './Components/AdminCheck';
 import AddLocation from './Components/AddLocation';
 import EditLocation from './Components/EditLocation';
 import PrivateRoute from './Components/PrivateRoutes';
 
-
+/**
+ * The main app component
+ * @returns view of the app
+ */
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* Hides the search bar and navbar when on the login screen. */}
         <Routes>
           <Route exact path="/login" element={<Login />}/>
           <Route exact path="/AddLoc" element={<PrivateRoute Component={AddLocation}/>} />
           <Route exact path="/EditLoc/*" element={<PrivateRoute Component={EditLocation} />} />
           <Route exact path="" element={<Navigate to="/map" />}/> {/* Redirect to map if no path is specified */}
           <Route exact path="/map" element={<PrivateRoute Component={MapView}/>} />
-          <Route exact path="/adminTest" element={<AdminTest />} />
         </Routes>
       </div>
     </Router>
